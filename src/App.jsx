329981ada8a75093c57cfa558bc7cce45779eaf5
jatin.acharya.ext <jatin.acharya.ext@veolia.com>
2025-07-27 03:11:48 +0530

    general styling, title, instructions added

--- src/App.jsx
+++ src/App.jsx
@@ -44,8 +44,12 @@ function App() {
   return (
     <>
       <main>
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
         <section className="dice-section">{dieElements}</section>
-
         <button className="roll-button" onClick={handleDiceRoll}>
           Roll
         </button>
