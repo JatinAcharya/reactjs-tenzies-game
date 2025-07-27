36b12bb865e60c0fca11dbc86746244e51ae3d35
jatin.acharya.ext <jatin.acharya.ext@veolia.com>
2025-07-27 17:37:27 +0530

    accessibility improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 import Die from "./components/Die";
 import Confetti from "react-confetti";
@@ -7,6 +7,7 @@ import { nanoid } from "nanoid";
 function App() {
   //added a callback function to initialization because earlier react was calling generateAllNewDice() on every render but by donig this way is will just call the generateAllNewDice() function the first time only
   const [dice, setDice] = useState(() => generateAllNewDice()); //[{value, isHeld}, {value, isHeld}]
+  const newGameButton = useRef(null);
 
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() => ({
@@ -49,17 +50,32 @@ function App() {
     dice.every((die) => die.isHeld) &&
     dice.every((die) => die.value == dice[0].value);
 
+  useEffect(() => {
+    if (gameWon) {
+      newGameButton.current.focus();
+    }
+  }, [gameWon]);
+
   return (
     <>
       <main>
         {gameWon && <Confetti />}
+        <div aria-live="polite" className="sr-only">
+          {gameWon && (
+            <p>Congratulations! You won! Press 'New Game' to start again.</p>
+          )}
+        </div>
         <h1 className="title">Tenzies</h1>
         <p className="instructions">
           Roll until all dice are the same. Click each die to freeze it at its
           current value between rolls.
         </p>
         <section className="dice-section">{dieElements}</section>
-        <button className="roll-button" onClick={handleDiceRoll}>
+        <button
+          className="roll-button"
+          onClick={handleDiceRoll}
+          ref={newGameButton}
+        >
           {gameWon ? "New Game" : "Roll"}
         </button>
       </main>
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -8,6 +8,10 @@ const Die = (props) => {
           props.dieObj.isHeld ? "die-button die-button-active" : "die-button"
         }
         onClick={() => props.dieClicked(props.dieObj.id)}
+        aria-pressed={props.dieObj.isHeld}
+        aria-label={`Die with value ${props.dieObj.value}, ${
+          props.dieObj.isHeld ? "held" : "not held"
+        }`}
       >
         {props.dieObj.value}
       </button>
